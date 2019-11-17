22b2c9c647c08e23d5effb006ac9ded826132411
Joyce Chin <joycechin0@gmail.com>
2019-11-17 19:13:27 +0800

    First commit

---
+++ UserInput/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static UserInput.UserInput;
+
+namespace UserInput
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            TextInput input = new NumericInput();
+            input.Add('1');
+            input.Add('a');
+            input.Add('0');
+            Console.WriteLine(input.GetValue());
+        }
+    }
+}
---
+++ UserInput/UserInput.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace UserInput
+{
+    public class UserInput
+    {
+        public abstract class TextInput
+        {
+            public StringBuilder sb = new StringBuilder();
+
+            public virtual void Add(char c)
+            { 
+                sb.Append(c);
+            }
+
+            public string GetValue()
+            {
+                return sb.ToString();
+            }
+        }
+
+        public class NumericInput : TextInput
+        {
+            public override void Add(char a)
+            {
+                if (Char.IsDigit(a))
+                {
+                    sb.Append(a);
+                }
+            }
+        }
+    }
+}
